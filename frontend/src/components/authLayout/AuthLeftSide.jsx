e74492b5cc0ccf19eaed646f5149389f5e19c9e2
abdullahasad45 <abdullah.asad@vaival.tech>
2026-06-21 20:12:34 +0500

    version1

--- frontend/src/components/authLayout/AuthLeftSide.jsx
+++ frontend/src/components/authLayout/AuthLeftSide.jsx
@@ -7,7 +7,7 @@ const AuthLeftSide = () => {
       {/* Left Side Branding */}
       <div className="w-1/2 bg-black flex flex-col items-center justify-center px-10">
         <h2 className="text-white text-4xl font-bold mb-4 text-center">
-          Welcome to Mubasher Industries
+          docker-pipeline 
         </h2>
         <p className="text-gray-400 text-lg text-center">
           Your gateway to excellence and innovation.
